87226579f33722b59452446b2ef4444f545076d9
Ramiro <raminp.1012@gmail.com>
2022-03-10 19:45:12 -0500

    primer commit

--- src/main/java/com/megafact/model/LicenciaFuncionamiento.java
+++ src/main/java/com/megafact/model/LicenciaFuncionamiento.java
@@ -3,13 +3,20 @@ package com.megafact.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "licencia_funcionamiento")
 public class LicenciaFuncionamiento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idLicencia;
+
+    @Column(name = "nombre_razon_social", nullable = false, length = 60)
+    private String nombre_razon_social;
+
 
 }
