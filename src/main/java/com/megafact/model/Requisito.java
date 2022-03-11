865c92b9c517d87e11f6e952440ecfacc9d3bd68
jhon najarro <jhonfrank21@hotmail.com>
2022-03-10 19:29:45 -0500

    2da

--- src/main/java/com/megafact/model/Requisito.java
+++ src/main/java/com/megafact/model/Requisito.java
@@ -1,11 +1,16 @@
 package com.megafact.model;
 
 import com.sun.tools.javac.comp.Resolve;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Getter
+@Setter
 @Entity
 @Table(name="requisito")
 public class Requisito {
