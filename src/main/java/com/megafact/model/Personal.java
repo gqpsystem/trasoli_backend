3c117f950e18dac29d8b0d07b7a7729c860f7bdd
Jcrlos31 <jcjave31@gmail.com>
2022-03-10 19:08:42 -0500

    Juan Carlos

--- src/main/java/com/megafact/model/Personal.java
+++ src/main/java/com/megafact/model/Personal.java
@@ -5,5 +5,14 @@ import javax.persistence.*;
 @Table(name = "personal")
 public class Personal {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPersonal;
+
+    @Column(name = "nombrePersonal",nullable = false,length = 100)
+    private String nombreCliente;
+
+    @Column(name = "apellidosPersonal",nullable = false,length = 100)
+    private String apellidosPersonal;
 
 }
