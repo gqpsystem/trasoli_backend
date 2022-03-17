3bb481e8a4be780ac0fea5b29df5aee2f0fe8137
Grimaldo <gqpsystem@gmail.com>
2022-03-16 19:54:37 -0500

    commit 05

--- src/main/java/com/megafact/repository/IRequisitoDAO.java
+++
@@ -1,9 +0,0 @@
-package com.megafact.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.megafact.model.Requisito;
-
-public interface IRequisitoDAO extends JpaRepository<Requisito,Long> {
-
-}
