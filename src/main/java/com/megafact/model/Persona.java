87ad7eea4915f8a50aa99d651ceb4467cbdb1c45
Grimaldo <gqpsystem@gmail.com>
2022-03-16 20:06:23 -0500

    commit 06

--- src/main/java/com/megafact/model/Persona.java
+++ src/main/java/com/megafact/model/Persona.java
@@ -1,4 +1,17 @@
 package com.megafact.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "persona")
 public class Persona {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idPersona;
 }
