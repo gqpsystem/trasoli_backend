c361ade9cc2198ae50c31ef17ae6831de4227f24
Grimaldo <gqpsystem@gmail.com>
2022-03-16 20:12:37 -0500

    commit-grimaldo

--- src/main/java/com/megafact/model/Persona.java
+++ src/main/java/com/megafact/model/Persona.java
@@ -14,4 +14,10 @@ public class Persona {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idPersona;
+
+    @Column(name = "nombre", nullable = false, length = 200)
+    private String nombre;
+
+    @Column(name = "numeroDocumento", nullable = false, length = 11)
+    private String numeroDocumento;
 }
