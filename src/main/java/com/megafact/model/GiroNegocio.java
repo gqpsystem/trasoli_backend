a4ac2995714f1c18f1d144ef0418de15e0242148
jhon najarro <jhonfrank21@hotmail.com>
2022-03-16 22:54:39 -0500

    16_jhon_giroNegocio

---
+++ src/main/java/com/megafact/model/GiroNegocio.java
@@ -0,0 +1,16 @@
+package com.megafact.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "giro_negocio")
+public class GiroNegocio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
