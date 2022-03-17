846a297caa965071474e6ed3b754f6dd172b0419
Grimaldo <gqpsystem@gmail.com>
2022-03-17 18:48:32 -0500

    commit-grimaldo-06

---
+++ src/main/java/com/megafact/service/IPersonalService.java
@@ -0,0 +1,12 @@
+package com.megafact.service;
+
+import com.megafact.model.Personal;
+import com.megafact.model.TipoDocumento;
+
+import java.util.List;
+
+public interface IPersonalService {
+
+    List<Personal> listar();
+
+}
