5701ba2fc2f7ea48cd282585049abc5cffc7a426
jhon najarro <jhonfrank21@hotmail.com>
2022-03-17 19:02:51 -0500

    jhon-establecimiento_v1

--- src/main/java/com/megafact/model/Establecimiento.java
+++ src/main/java/com/megafact/model/Establecimiento.java
@@ -14,28 +14,28 @@ public class Establecimiento {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idEstablecimiento;
 
-    @Column(name = "codigo_establecimiento")
+    @Column(name = "codigo_establecimiento", nullable = false,length = 20)
     private String codigoEstablecimiento;
 
-    @Column(name = "nombre_establecimiento")
+    @Column(name = "nombre_establecimiento", nullable = false,length = 100)
     private String nombreEstablecimiento;
 
-    @Column(name = "telefono")
+    @Column(name = "telefono", nullable = false,length = 9)
     private String telefono;
 
-    @Column(name = "direccion")
+    @Column(name = "direccion", nullable = false,length = 60)
     private String direccion;
 
-    @Column(name = "departamento")
+    @Column(name = "departamento" , nullable = false,length = 50)
     private String departamento;
 
-    @Column(name = "provincia")
+    @Column(name = "provincia", nullable = false,length = 50)
     private String provincia;
 
-    @Column(name = "distrito")
+    @Column(name = "distrito", nullable = false,length = 50)
     private String distrito;
 
-    @Column(name = "zona")
+    @Column(name = "zona" , nullable = false,length = 40)
     private String zona;
 
 
