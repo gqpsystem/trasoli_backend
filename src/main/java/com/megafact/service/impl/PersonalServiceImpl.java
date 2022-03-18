48b18bda67364ad93213e524a54b75e0f1fb9f13
Grimaldo <gqpsystem@gmail.com>
2022-03-17 20:19:54 -0500

    commit-grimaldo-10

--- src/main/java/com/megafact/service/impl/PersonalServiceImpl.java
+++ src/main/java/com/megafact/service/impl/PersonalServiceImpl.java
@@ -5,10 +5,8 @@ import com.megafact.repository.IPersonalDAO;
 import com.megafact.service.IPersonalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class PersonalServiceImpl implements IPersonalService {
@@ -18,19 +16,17 @@ public class PersonalServiceImpl implements IPersonalService {
 
     @Override
     public List<Personal> listar() {
-
         return dao.findAll();
     }
 
     @Override
     public Personal crearPersonal(Personal personal) {
-        Optional<Personal> guardar = dao.findById(personal.getIdPersonal());
         return dao.save(personal);
     }
 
     @Override
-    public Optional<Personal> mostrarPersonalPorId(long id) {
-        return dao.findById(id);
+    public Personal mostrarPersonalPorId(long idPersonal) {
+        return dao.findById(idPersonal).orElse(null);
     }
 
     @Override
@@ -39,8 +35,8 @@ public class PersonalServiceImpl implements IPersonalService {
     }
 
     @Override
-    public void eliminarPersonal(long id) {
-        dao.deleteById(id);
+    public void eliminarPersonal(Personal personal) {
+        dao.save(personal);
     }
 
 }
