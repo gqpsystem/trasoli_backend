47d237297acb459b8985b8a4a5ec7480674e6402
Oscar MENESES YARANGA <oscar.meya@gmail.com>
2022-03-31 20:47:03 -0500

    Commit - Oscar 9

--- src/main/java/com/megafact/config/SwaggerConfig.java
+++ src/main/java/com/megafact/config/SwaggerConfig.java
@@ -2,14 +2,15 @@ package com.megafact.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+//import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+import springfox.documentation.builders.ApiInfoBuilder;
+import static springfox.documentation.builders.PathSelectors.regex;
 import java.util.Collections;
 
 @Configuration
@@ -18,22 +19,25 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.megafact.controller"))
-                .paths(PathSelectors.regex("/api.*"))
-                .build()
-                .apiInfo(apiInfo());
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.megafact.controller"))
+//                .paths(PathSelectors.regex("/api.*"))
+//                .build()
+//                .apiInfo(apiInfo());
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).pathMapping("/").select()
+                .paths(regex("/api.*")).build();
     }
 
 
     private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "REST API MEGAFACT",
-                "SISTEMA DE TRAMITE DE SOLICITUD DE LICENCIA",
-                "API V1.1",
-                "http://localhost:9091/swagger-ui.html#/",
-                new Contact("MegaFact", "https://www.facebook.com/megafacturacion/", "gqpsystem@gmail.com"),
-                "Open Source", "\"https://www.apache.org/licenses/LICENSE-2.0", Collections.emptyList());
+//        return new ApiInfo(
+//                "REST API MEGAFACT",
+//                "SISTEMA DE TRAMITE DE SOLICITUD DE LICENCIA",
+//                "API V1.1",
+//                "http://localhost:9091/swagger-ui.html#/",
+//                new Contact("MegaFact", "https://www.facebook.com/megafacturacion/", "gqpsystem@gmail.com"),
+//                "Open Source", "\"https://www.apache.org/licenses/LICENSE-2.0", Collections.emptyList());
+        return new ApiInfoBuilder().title("REST API MEGAFACT").description("SISTEMA DE TRAMITE DE SOLICITUD DE LICENCIA").version("1.0").build();
     }
 }
--- src/main/java/com/megafact/controller/SolicitudController.java
+++ src/main/java/com/megafact/controller/SolicitudController.java
@@ -1,11 +1,16 @@
 package com.megafact.controller;
 
+import com.megafact.model.Solicitud;
 import com.megafact.service.ISolicitudService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value= "/api/solicitudes")
@@ -19,4 +24,54 @@ public class SolicitudController {
         return "{ \"todoOk\" : true }";
     }
 
+
+    //@GetMapping(value = "/list-solicitud",produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping
+    public ResponseEntity<?> listar() {
+        List<Solicitud> list = new ArrayList<>();
+        try {
+            list = service.listar();
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    /*
+    {
+    "asunto":"Solicito nueva licencia de funcionamiento",
+    "estado":true,
+    "fechaRecepcion":"2022-03-31",
+    "numExpediente":"001-2022",
+    "establecimiento":{
+        "idEstablecimiento":2
+        },
+    "licencia":{
+        "idLicencia":1
+        }
+    }
+     */
+    //@PostMapping(value = "/registrar", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping
+    public ResponseEntity<Object> registrar(@RequestBody Solicitud solicitud) {
+        try {
+            Solicitud solicitud1 = service.crearSolicitud(solicitud);
+            return new ResponseEntity<>(solicitud1, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping( path = "/{id}")
+    public Solicitud obtenerEmpresaPorId(@PathVariable("id") Long id) {
+        return this.service.mostrarSolicitudPorId(id);
+    }
+
+    @DeleteMapping( path = "/{id}")
+    public String eliminarPorId(@PathVariable("id") Long id){
+        Solicitud solicitud= new Solicitud();
+        solicitud.setIdSolicitud(id);
+        this.service.eliminarSolicitud(solicitud);
+        return "Eliminado";
+    }
 }
